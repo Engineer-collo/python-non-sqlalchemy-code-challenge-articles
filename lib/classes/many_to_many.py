35ca1afffe6892c5bb17d7219963dcc4695ea875
Engineer-Collo <collinslikhomba@gmail.com>
2025-02-28 15:32:33 +0300

    updated tests

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,54 +1,39 @@
-# Class Author
 class Author:
-    all_authors = []  # Store all authors
-
     def __init__(self, name):
-        self._name = name  # Using a private variable
-        Author.all_authors.append(self)
+        self._name = name
+        self.name = name
 
     @property
     def name(self):
-        return self._name  # Return the private variable
+        return self._name
 
     @name.setter
-    def name(self, name):
-        if hasattr(self, "_name"):
-            raise Exception("Can't reassign the name")
-        elif isinstance(name, str) and len(name) > 0:
-            self._name = name
+    def name(self, value):
+        if not hasattr(self, "_name"):  # Name should only be set once
+            if isinstance(value, str) and len(value) > 0:
+                self._name = value
 
-    def authors_written_articles(self):
+    def articles(self):
         return [article for article in Article.all_articles if article.author == self]
 
-    def magazines_contributed(self):
-        magazines = [article.magazine for article in Article.all_articles if article.author == self]
-        return set(magazines)
+    def magazines(self):
+        return list(set(article.magazine for article in self.articles()))
 
     def add_article(self, magazine, title):
-        return Article(author=self, magazine=magazine, title=title)
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        categories = []
-        for article in Article.all_articles:
-            if article.author == self and article.magazine.category not in categories:
-                categories.append(article.magazine.category)
-        return categories if categories else None
+        return list(set(magazine.category for magazine in self.magazines())) if self.magazines() else None
 
 
-
-
-
-
-
-
-
-# Class Magazine
 class Magazine:
     all_magazines = []  # Store all magazines
 
     def __init__(self, name, category):
         self._name = name
         self._category = category
+        self.name = name
+        self.category = category
         Magazine.all_magazines.append(self)
 
     @property
@@ -69,37 +54,29 @@ class Magazine:
         if isinstance(category, str) and len(category) > 0:
             self._category = category
 
-    def articles_published(self):
+    def articles(self):
         return [article for article in Article.all_articles if article.magazine == self]
 
-    def authors(self):
-        authors = [article.author for article in Article.all_articles if article.magazine == self]
-        return set(authors)
+    def contributors(self):
+        return list(set(article.author for article in self.articles()))
 
     def article_titles(self):
-        titles = [article.title for article in Article.all_articles if article.magazine == self]
+        titles = [article.title for article in self.articles()]
         return titles if titles else None
 
     def contributing_authors(self):
         author_counts = {}
-        for article in Article.all_articles:
-            if article.magazine == self:
-                author_counts[article.author] = author_counts.get(article.author, 0) + 1
+        for article in self.articles():
+            author_counts[article.author] = author_counts.get(article.author, 0) + 1
         contributors = [author for author, count in author_counts.items() if count > 2]
         return contributors if contributors else None
 
 
-
-
-
-
-
-
-# Class Article
 class Article:
     all_articles = []  # Store all articles
 
     def __init__(self, author, magazine, title):
+        self._title = title
         self.author = author
         self.magazine = magazine
         self.title = title
@@ -111,31 +88,5 @@ class Article:
 
     @title.setter
     def title(self, title):
-        if hasattr(self, "_title"):
-            raise Exception("Can't reassign title")
-        elif isinstance(title, str) and 5 <= len(title) <= 50:
+        if isinstance(title, str) and len(title) > 0:
             self._title = title
-        else:
-            raise ValueError("Title must be a string with 5 to 50 characters")
-
-    @property
-    def author(self):
-        return self._author
-
-    @author.setter
-    def author(self, author):
-        if not isinstance(author, Author):
-            raise TypeError("author must be an instance of Author")
-        self._author = author
-
-    @property
-    def magazine(self):
-        return self._magazine
-
-    @magazine.setter
-    def magazine(self, magazine):
-        if not isinstance(magazine, Magazine):
-            raise TypeError("magazine must be an instance of Magazine")
-        self._magazine = magazine
-
-
